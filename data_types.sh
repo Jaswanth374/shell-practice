04640a7c4cd5f5bec2854391c4228618485141de
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-01-28 17:23:22 +0530

    commit message

--- data_types.sh
+++ data_types.sh
@@ -4,3 +4,11 @@ Num1=100
 Num2=200
 Total=$(($Num1 + $Num2))
 echo "Total value is :: $Total"
+
+#Arrays
+Furits=("Apple" "Banana" "Mango")
+
+echo "Fruits are: ${Fruits[@]}"
+echo "First fruit is :: ${Fruits[1]}"
+echo "Second Fruit is:: ${Fruit[2]}"
+echo "Third Fruit is:: ${Fruit[3]}"
