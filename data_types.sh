0d4bcb17ae3dd09382a106ead0082fd19c887a23
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-01-28 17:24:50 +0530

    commit message

--- data_types.sh
+++ data_types.sh
@@ -9,6 +9,6 @@ echo "Total value is :: $Total"
 Furits=("Apple" "Banana" "Mango")
 
 echo "Fruits are: ${Fruits[@]}"
-echo "First fruit is :: ${Fruits[1]}"
-echo "Second Fruit is:: ${Fruit[2]}"
-echo "Third Fruit is:: ${Fruit[3]}"
+echo "First fruit is :: ${Fruits[0]}"
+echo "Second Fruit is:: ${Fruit[1]}"
+echo "Third Fruit is:: ${Fruit[2]}"
