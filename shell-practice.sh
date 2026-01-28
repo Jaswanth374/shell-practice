096eb8aaaf089fe513118560752f81976e310b45
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-01-28 16:33:57 +0530

    commit message

---
+++ shell-practice.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "$1:: HI $2, How are you?"
+echo "$2:: Hi $1, I am good. How are you?"
+echo "I am good. Which course you are taking $2"
+echo "I am taking a devops course, Which is really good $1"
