6e9e1e48e8aaa51079c19d90578d78dc28f9e8a8
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-01-28 19:25:05 +0530

    commit message

--- installation.sh
+++ installation.sh
@@ -2,7 +2,8 @@
 userid=$(id -u)
 if [ $userid -e 0 ]; then
 echo "print the exit code:: $?"
-dnf install nginx
+dnf install nginx -y
+exit 1
 else
 echo "Not having a root access to execute. exit code return as:: $?"
 fi
