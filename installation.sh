ca0c387a1cc9666b884360ee9cac26f0bf018892
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-01-28 19:12:12 +0530

    commit message

--- installation.sh
+++ installation.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
+userid=$(id -u)
+if [$userid -ne 0]; then
+echo "print the exit code:: $?"
 dnf install nginx
+else
+echo "Not having a correct exit code to exec the install command:: $?"
+fi
