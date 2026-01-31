86e11cfd62ee86e379f51ada9c1a7da838cb7635
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-01-31 05:48:59 +0530

    commit message

--- Functions.sh
+++ Functions.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 #writing a shell script to use functions
 
+R='\e[0;31m'
+G='\e[0;32m'
+N='\e[0m'
 userid=$(id -u)
 if [ $userid -ne 0 ]; then
-echo "User is not having root access to install"
+echo "$R User is not having root access to install $N"
+exit 1
 else
-echo "User have access to install required softwares"
+echo "$G User have access to install required softwares $N"
 fi
 
 FUNC(){
     if [ $1 -ne 0 ]; then
-    echo "$2 ----> SUCCESS"
+    echo "$2 ----> $R FAILURE $N"
     else
-    echo "$2 ----> FAILURE"
+    echo "$2 ----> $G SUCCESS $N"
     fi
 }
 
