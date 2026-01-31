767c0c9403b9f5019de4cd16a592e5a1497eb3ee
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-01-31 05:40:26 +0530

    commit message

---
+++ Functions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#writing a shell script to use functions
+
+userid=$(id -u)
+if [ $userid -ne 0 ]; then
+echo "User is not having root access to install"
+else
+echo "User have access to install required softwares"
+fi
+
+FUNC() {
+    if [ $1 -ne 0 ]; then
+    echo "$2 ----> SUCCESS"
+    else
+    echo "$2 ----> FAILURE"
+}
+
+dnf install nginx -y
+FUNC $? "Installing the nginx"
+dnf install mysql -y
+FUNC $? "Installing the mysql"
